eb98ea2d87eb054dcae5d80385a14f6912a04824
kuramacode <sashaziuz@gmail.com>
2026-03-24 20:24:28 +0200

    to start

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, a
 from models import db, User, Transactions, Categories
 from config import Config
 from utils.jinja_filters import color_change, format_date, category_name, category_emoji
+from utils.main_scripts import *
 # Імпорт функцій для управління автентифікацією та сесіями користувачів
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 # Імпорт функцій для безпечного хешування та перевірки паролів
@@ -33,91 +34,6 @@ def load_user(user_id): # Ця функція викликається Flask-Log
     # Запит до бази даних: отримати користувача з заданим ID
     # int(user_id) - конвертація рядка ID в число
     return User.query.get(int(user_id)) 
-
-def get_userid(): # Функція для отримання ID поточного користувача (якщо він аутентифікований) або None (якщо користувач не аутентифікований) 
-    if current_user and current_user.is_authenticated: # Перевіряє, чи є поточний користувач та чи він аутентифікований
-        return current_user.id # Повертає ID поточного користувача, якщо він аутентифікований
-    return None # Повертає None, якщо користувач не аутентифікований або відсутній
-
-def get_username():
-    return current_user.username
-
-def get_transactions(userId):
-    database = sqlite3.connect(Config.SQLALCHEMY_DATABASE_URI.replace('sqlite:///', '')) # Підключення до бази даних SQLite за допомогою шляху, визначеного в конфігурації
-    database.row_factory = sqlite3.Row
-    cur = database.cursor()
-
-    cur.execute('''SELECT * FROM transactions
-                WHERE user_id = ?
-                ORDER BY date DESC
-                ''' , (userId,))
-
-    transactions = cur.fetchall()
-    database.close()
-
-    return transactions
-
-def get_last_transactions(userId):
-    database = sqlite3.connect(Config.SQLALCHEMY_DATABASE_URI.replace('sqlite:///', '')) # Підключення до бази даних SQLite за допомогою шляху, визначеного в конфігурації
-    database.row_factory = sqlite3.Row
-    cur = database.cursor()
-
-    cur.execute('''SELECT * FROM transactions
-                WHERE user_id = ?
-                ORDER BY date DESC
-                LIMIT 5
-                ''', (userId,))
-    transactions = cur.fetchall()
-    database.close()
-
-    return transactions
-
-def get_sum_income(userId):
-    database = sqlite3.connect(Config.SQLALCHEMY_DATABASE_URI.replace('sqlite:///', '')) # Підключення до бази даних SQLite за допомогою шляху, визначеного в конфігурації
-    database.row_factory = sqlite3.Row
-    cur = database.cursor()
-    type = 'income'
-
-    cur.execute('''SELECT * FROM transactions WHERE user_id = ?''', (userId,))
-    list = cur.fetchall()
-    if len(list) > 0:
-        cur.execute('''SELECT SUM(amount) FROM transactions
-                    WHERE user_id = ? AND type = ?''', (userId, type,))
-        sum = cur.fetchall()
-
-        return abs(sum[0][0])
-    return 0
-
-def get_sum_expense(userId):
-    database = sqlite3.connect(Config.SQLALCHEMY_DATABASE_URI.replace('sqlite:///', '')) # Підключення до бази даних SQLite за допомогою шляху, визначеного в конфігурації
-    database.row_factory = sqlite3.Row
-    cur = database.cursor()
-    type = 'expense'
-
-    cur.execute('''SELECT * FROM transactions WHERE user_id = ?''', (userId,))
-    list = cur.fetchall()
-    if len(list) > 0:
-        cur.execute('''SELECT SUM(amount) FROM transactions
-                    WHERE user_id = ? AND type = ?''', (userId, type,))
-        sum = cur.fetchall()
-        return abs(sum[0][0])
-    return 0
-
-def filter_transactions(user_id, type):
-    database = sqlite3.connect(Config.SQLALCHEMY_DATABASE_URI.replace('sqlite:///', '')) # Підключення до бази даних SQLite за допомогою шляху, визначеного в конфігурації
-    database.row_factory = sqlite3.Row
-    cur = database.cursor()
-
-    if type == 'all':
-        cur.execute('''SELECT * FROM transactions WHERE user_id=?''', (user_id,))
-        filtered = cur.fetchall()
-    else:
-        cur.execute('''SELECT * FROM transactions
-                    WHERE user_id = ? AND type = ?''', (user_id, type,))
-        filtered = cur.fetchall()
-    database.close()
-
-    return filtered
 
 # Контекст додатка необхідний для роботи з базою даних поза межами обробки запитів
 with app.app_context():
